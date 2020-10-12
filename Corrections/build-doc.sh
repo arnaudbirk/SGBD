d8ac67ad67086341fc9d0c9fdae33c14832d1186
Arnaud Birk <arnaud.birk@ign.fr>
2020-10-12 11:37:07 +0200

    Ajout Correction TP Films

--- Corrections/build-doc.sh
+++ Corrections/build-doc.sh
@@ -3,4 +3,5 @@ pandoc -s -o Modelisation/Correction_Association_de_randonneurs.pdf Modelisation
 pandoc -s -o Modelisation/Correction_clés_primaires_et_étrangères.pdf Modelisation/Correction_clés_primaires_et_étrangères.md
 pandoc -s -o Modelisation/Correction_Compagnie_aérienne.pdf Modelisation/Correction_Compagnie_aérienne.md
 # SQL
-pandoc -s -o SQL/Correction_SQL.pdf SQL/Correction_SQL.md
+pandoc -s -o SQL/Villes/Correction_Villes.pdf SQL/Villes/Correction_Villes.md
+pandoc -s -o SQL/Films/Correction_Films.pdf SQL/Films/Correction_Films.md
